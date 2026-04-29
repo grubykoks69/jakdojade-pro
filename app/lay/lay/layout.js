4ea7f0681db988a9ce40967be4ecab2b91990c68
grubykoks69 <adrianflaszka@gmail.com>
2026-04-29 21:30:13 +0200

    Update and rename jakdojade-pro to app/lay/lay/layout.js

---
+++ app/lay/lay/layout.js
@@ -0,0 +1,16 @@
+{
+  "name": "jakdojade-pro",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "14.0.0",
+    "react": "18.2.0",
+    "react-dom": "18.2.0",
+    "react-leaflet": "^4.2.0",
+    "leaflet": "^1.9.0"
+  }
+}
